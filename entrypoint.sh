9a3558133a7273b02c90458d94be95f265ff735d
doislong <15221580643@163.com>
2025-11-07 14:52:23 +0800

    fix entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # 将变量注入 nginx.conf
-envsubst '$SOP_API_HOST $CHAT_API_HOST' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '$SOP_API_HOST $CHAT_API_HOST $COMPANY_API_HOST $CHAT_HISTORY_API_HOST $USER_API_HOST' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 # 启动 nginx
 nginx -g 'daemon off;'
