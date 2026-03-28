aa4f913d85924f92545767906b33fab33d43cc70
notAugust <1209982223@qq.com>
2026-03-28 15:27:17 +0800

    fix: 配置项兼容

---
+++ public/eap/assets/config.DIR81T5l.js
@@ -0,0 +1,1 @@
+let a,s,e,i,p,t;a="/userapi",s="/mobileapi",e="/sop-api",i="http://210.22.113.18:19080/ai/ai-learn-index.html",p="/chat/sop",t="/h5Preview";export{i as A,a as B,t as P,e as S,p as T,s as m};
