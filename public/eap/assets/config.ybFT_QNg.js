1838ad8cb2f5e57b921105e6bb8453b0aec57917
notAugust <1209982223@qq.com>
2026-04-01 15:18:56 +0800

    fix: h5更改

---
+++ public/eap/assets/config.ybFT_QNg.js
@@ -0,0 +1,1 @@
+let a,i,s,e,p,t;a="/userapi",i="/mobileapi",s="/sop-api",e="https://ai.cuitong.cc:19082/ai/ai-learn-index.html",p="/chat/sop",t="/h5Preview";export{e as A,a as B,t as P,s as S,p as T,i as m};
